71739b299c5a991554f09b34c1274fbf68c61268
LuRoGlez <rosell.golui20@cadiz.salesianos.edu>
2021-04-12 12:05:59 +0200

    modelos clientes y usuarios

--- yoTeAyudo/nucleo/models.py
+++ yoTeAyudo/nucleo/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 
 
+class Usuario(models.Model):
+    username=models.CharField(max_length=30)
+    password=models.IntegerField(max_length=30)
+
 class Cliente (models.Model):
     dni=models.CharField(max_length=9)
     nombre=models.CharField(max_length=30)
@@ -8,4 +12,4 @@ class Cliente (models.Model):
     direccion=models.CharField(max_length=80)
     fechaNacimiento=models.DateField(max_length=30)
     foto=models.ImageField(upload_to='photos/', verbose_name="Foto")
-    idUsuario=models.OneToOneField(User, on_delete=models.CASCADE)
+    idUsuario=models.OneToOneField(Usuario, on_delete=models.CASCADE)
