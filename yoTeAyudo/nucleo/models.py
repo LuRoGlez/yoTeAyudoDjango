efc4dcbdc4cedb71cf0738329750329a8f17052c
LuRoGlez <rosell.golui20@cadiz.salesianos.edu>
2021-04-12 11:59:31 +0200

    modelo clientes

--- yoTeAyudo/nucleo/models.py
+++ yoTeAyudo/nucleo/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Cliente (models.Model):
+    dni=models.CharField(max_length=9)
+    nombre=models.CharField(max_length=30)
+    apellidos=models.CharField(max_length=30)
+    direccion=models.CharField(max_length=80)
+    fechaNacimiento=models.DateField(max_length=30)
+    foto=models.ImageField(upload_to='photos/', verbose_name="Foto")
+    idUsuario=models.OneToOneField(User, on_delete=models.CASCADE)
