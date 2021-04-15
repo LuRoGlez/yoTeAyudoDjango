bc8870024ed379d4e785d599064c56c702ce1beb
LuRoGlez <rosell.golui20@cadiz.salesianos.edu>
2021-04-15 10:42:55 +0200

    solucionados error modelos y str

--- yoTeAyudo/nucleo/models.py
+++ yoTeAyudo/nucleo/models.py
@@ -12,34 +12,34 @@ class Cliente (models.Model):
     nombre=models.CharField(max_length=30)
     apellidos=models.CharField(max_length=50)
     direccion=models.CharField(max_length=100)
-    fechaNacimiento=models.DateField
+    fechaNacimiento=models.DateField(null=True)
     foto=models.ImageField(upload_to='photos/', verbose_name="Foto")
     idUsuario=models.OneToOneField(Usuario, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.dni+" "+self.nombre+" "+self.apellidos+" "+self.direccion+" "+self.fechaNacimiento+" "+self.foto
+        return self.dni+" "+self.nombre+" "+self.apellidos+" "+self.direccion
 
 class Especialista (models.Model):
     dni=models.CharField(max_length=9)
     nombre=models.CharField(max_length=30)
     apellidos=models.CharField(max_length=30)
     direccion=models.CharField(max_length=80)
-    fechaNacimiento=models.DateField
+    fechaNacimiento=models.DateField(null=True)
     foto=models.ImageField(upload_to='photos/', verbose_name="Foto")
     biografia=models.TextField(max_length=255)
     idUsuario=models.OneToOneField(Usuario, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.dni+" "+self.nombre+" "+self.apellidos+" "+self.direccion+" "+self.fechaNacimiento+" "+self.foto+" "+self.biografia
+        return self.dni+" "+self.nombre+" "+self.apellidos+" "+self.direccion+" "+self.biografia
 
 
 class Cita(models.Model):
-    fecha=models.DateField
+    fecha=models.DateField(null=True)
     idCliente=models.ForeignKey(Cliente, verbose_name="Cliente", on_delete=models.CASCADE, related_name='Cliente')
     idEspecialista=models.ForeignKey(Especialista, verbose_name="Especialista", on_delete=models.CASCADE, related_name='Especialista')
 
     def __str__(self):
-        return self.fecha+" Cliente: "+self.idCliente.nombre+" "+self.idCliente.apellidos+" Especialista: "+self.idEspecialista.nombre+" "+self.idEspecialista.apellidos
+        return " Cliente: "+self.idCliente.nombre+" "+self.idCliente.apellidos+" Especialista: "+self.idEspecialista.nombre+" "+self.idEspecialista.apellidos
 
 class Mensaje(models.Model):
     idEmisor=models.ForeignKey(Usuario, verbose_name="Usuario Emisor", on_delete=models.CASCADE, related_name='Usuario_Emisor')
