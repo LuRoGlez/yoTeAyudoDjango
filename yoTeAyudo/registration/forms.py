bc3833b07f4b396f2d0ccf44e207ab9e65d8b94b
LuRoGlez <rosell.golui20@cadiz.salesianos.edu>
2021-05-04 13:17:01 +0200

    formulario de registro, falla guardar la pass

---
+++ yoTeAyudo/registration/forms.py
@@ -0,0 +1,23 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+
+class UserCreationFormWithEmail(UserCreationForm):
+    email=forms.EmailField(required=True,help_text="Requerido. 254 caracteres como maximo")
+
+    class Meta:
+        model=User
+        fields=('username','password1','password2','email')
+
+    def clean_email(self):
+        value=self.cleaned_data['email']
+        if User.objects.filter(email=value).exists():
+            raise forms.ValidationError("Email ya registrado, prueba otro")
+        return value
+
+    def save(self, commit=True):
+        user = super(UserCreationForm, self).save()
+        user.email = self.cleaned_data["email"]
+        user.save()
+        return user
--- yoTeAyudo/registration/urls.py
+++ yoTeAyudo/registration/urls.py
@@ -4,5 +4,5 @@ from django.urls import path
 from registration import views
 
 urlpatterns = [
-    
+     path('registro', views.SignupView.as_view(), name="registro"),
 ]
