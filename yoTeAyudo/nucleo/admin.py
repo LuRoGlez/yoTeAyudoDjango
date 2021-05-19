8642708a09da8a25fec3f02e5f17f8ce8c9b0abb
LuRoGlez <rosell.golui20@cadiz.salesianos.edu>
2021-05-19 09:07:39 +0200

    añadido user a admin.py

--- yoTeAyudo/nucleo/admin.py
+++ yoTeAyudo/nucleo/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from nucleo.models import Cliente, Especialista, Cita, Mensaje
+from nucleo.models import Cliente, Especialista, Cita, Mensaje, User
 
 # Register your models here.
 admin.site.register(Cliente)
 admin.site.register(Especialista)
 admin.site.register(Cita)
 admin.site.register(Mensaje)
+admin.site.register(User)
